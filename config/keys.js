0d3b438a150391c67cca9551f2514a2dc1586bc9
csandman <csandvik@uvm.edu>
2018-10-17 01:11:52 -0400

    updated config values

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongoURI: 'mongodb://lbbase_admin:password1@ds111565.mlab.com:11565/lbbase'
+    MONGO_URI: 'mongodb://lbbase_admin:password1@ds111565.mlab.com:11565/lbbase'
 }
