9ba121deb98fc4990914cf9be6cf337e6ada125a
csandman <csandvik@uvm.edu>
2018-10-23 03:24:49 -0400

    added get deck route

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,5 @@
 module.exports = {
-    MONGO_URI: 'mongodb://lbbase_admin:password1@ds111565.mlab.com:11565/lbbase'
-}
+  MONGO_URI: "mongodb://lbbase_admin:password1@ds111565.mlab.com:11565/lbbase",
+  AWS_ACCESS_KEY_ID: "AKIAI6SP6HTYPPMCU2SA",
+  AWS_SECRET_ACCESS_KEY: "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"
+};
