5af1889dfecc6e886800a937fce20d1fad369fa1
csandman <csandvik@uvm.edu>
2019-09-03 18:52:12 -0400

    fixed cors issue

--- server.js
+++ server.js
@@ -10,6 +10,22 @@ const app = express();
 // Bodyparser Middleware
 app.use(express.json());
 
+const allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+  // intercept OPTIONS method
+  if ('OPTIONS' == req.method) {
+    res.send(200);
+  }
+  else {
+    next();
+  }
+};
+
+app.use(allowCrossDomain);
+
 // DB Config
 const db = process.env.MONGO_URI;
 
